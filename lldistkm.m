a2ee2c7791d0d9dd10002bc01ac9bd643002456f
Dante Sanaei <44450060+dasanaei@users.noreply.github.com>
2019-06-27 13:03:04 -0400

    Remove Extra Comments

--- lldistkm.m
+++ lldistkm.m
@@ -15,31 +15,7 @@ function [d1km, d2km]=lldistkm(latlon1,latlon2)
 % --Outputs:
 %   d1km: distance calculated by Haversine formula
 %   d2km: distance calculated based on Pythagoran theorem
-%
-% --Example 1, short distance:
-%   latlon1=[-43 172];
-%   latlon2=[-44  171];
-%   [d1km d2km]=distance(latlon1,latlon2)
-%   d1km =
-%           137.365669065197 (km)
-%   d2km =
-%           137.368179013869 (km)
-%   %d1km approximately equal to d2km
-%
-% --Example 2, longer distance:
-%   latlon1=[-43 172];
-%   latlon2=[20  -108];
-%   [d1km d2km]=distance(latlon1,latlon2)
-%   d1km =
-%           10734.8931427602 (km)
-%   d2km =
-%           31303.4535270825 (km)
-%   d1km is significantly different from d2km (d2km is not able to work
-%   for longer distances).
-%
-% First version: 15 Jan 2012
-% Updated: 17 June 2012
-%--------------------------------------------------------------------------
+
 
 radius=6371;
 lat1=latlon1(1)*pi/180;
